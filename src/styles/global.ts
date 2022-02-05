c59c650f8cac19a6d4336d33316f9d9bf8b83b83
Vinicius Inocencio de Souza <vicnicius2009@gmail.com>
2022-02-04 22:48:09 -0300

    Adding Header, Transactions and Summary

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,12 +4,11 @@ export const GlobalStyle = createGlobalStyle`
     :root {
         --red: #e52e4d;
         --blue: #5429cc;
-
+        --green: #33cc95;
         --blue-light: #6933ff;
         
-        --text-light: #363f5f;
+        --text-title: #363f5f;
         --text-body: #969cb3;
-
 
         --background: #f0f2f5;
         --shape: #FFF;
@@ -34,6 +33,15 @@ export const GlobalStyle = createGlobalStyle`
     body {
         background-color: var(--background);
         -webkit-font-smoothing: antialiased; 
+    }
+
+    body, input, textarea, button {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+    }
+
+    h1, h2, h3, h4, h5, h6, strong {
+        font-weight: 600;
     }
 
     button {
